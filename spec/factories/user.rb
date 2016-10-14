e14b8f361e5762ad0fa30c2fd285e9f789be5593
danieladler <weston.adler@gmail.com>
2016-10-14 12:10:42 -0700

    start refactoring user specs to use factories

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
     username "user_0"
-    email "user_0@rc.com"
+    email { "#{username}@rc.com" }
+    password "secret"
+    password_confirmation "secret"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,27 +8,33 @@ describe User, type: :model do
         expect(user.valid?).to eq false
       end
     end
-  #
-  #   context "when email not unique" do
-  #     it "is invalid" do
-  #       user_2.update(email: user_1.email)
-  #       expect(user_2.valid?).to eq false
-  #     end
-  #   end
-  #
-  #   context "when email is missing '@'" do
-  #     it "is invalid" do
-  #       user_2.update(email: 'myemail')
-  #       expect(user_2.valid?).to eq false
-  #     end
-  #   end
-  #
-  #   context "when username is empty" do
-  #     it "is invalid" do
-  #       user_1.update(username: nil)
-  #       expect(user_1.valid?).to eq false
-  #     end
-  #   end
+
+    context "when email not unique" do
+      it "is invalid" do
+        user_1 = create(:user) # use `create` here to save user_1 to db...
+        user_2 = build(:user)  # ...and `build` here so that user_2 is NOT saved...
+        user_2.save            # ...so that when we DO save it, validation errors occur
+        expect(user_2.valid?).to eq false
+        # if we use `create` for user_2 then validation errors occur before the test can catch it.
+      end
+    end
+
+    context "when email is missing '@'" do
+      it "is invalid" do
+        user = build(:user, email: "invalid email")
+        user.save
+        expect(user.valid?).to eq false
+      end
+    end
+
+    context "when username is empty" do
+      it "is invalid" do
+        user = build(:user, username: nil)
+        user.save
+        puts user.errors.inspect
+        expect(user.valid?).to eq false
+      end
+    end
   #
   #   context "when username not unique" do
   #     it "is invalid" do

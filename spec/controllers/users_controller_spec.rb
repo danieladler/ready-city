752ca433182cff255659ec8ee498470a9b523abc
danieladler <weston.adler@gmail.com>
2016-10-14 14:44:21 -0700

    controller specs for GET #show

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,32 +10,60 @@ describe UsersController, type: :controller do
 
   describe "POST #create" do
     context "with valid attributes" do
+      let(:make_request) {
+        post :create, params: {user: attributes_for(:user)}
+      }
+
       it "creates a new user" do
-        expect {
-          post :create, params: {user: attributes_for(:user)}
-        }.to change(User, :count).by(1)
+        expect { make_request }.to change(User, :count).by(1)
       end
 
       it "redirects to the new user" do
-        post :create, params: {user: attributes_for(:user)}
+        make_request
         expect(response).to redirect_to User.last
       end
 
       it "returns a descriptive success message" do
-        post :create, params: {user: attributes_for(:user)}
+        make_request
         expect(flash[:success]).to include "Account created. Welcome to Ready City!"
       end
     end
 
     context "with invalid attributes" do
-      it "renders /sign-up template" do
+      let(:make_request) {
         post :create, params: {user: attributes_for(:user, username: nil)}
+      }
+
+      it "renders /sign-up template" do
+        make_request
         expect(response).to render_template :sign_up
       end
 
       it "returns a descriptive error message" do
-        post :create, params: {user: attributes_for(:user, username: nil)}
+        make_request
         expect(flash[:error]).to include "username: can't be blank"
+      end
+    end
+  end
+
+  describe "GET #show" do
+    context "with valid attributes" do
+      it "returns 200" do
+        user = create(:user)
+        get :show, params: { id: user.id }
+        expect(response.status).to eq 200
+      end
+
+      it "renders the #show view" do
+        user = create(:user)
+        get :show, params: { id: user.id }
+        expect(response).to render_template :show
+      end
+
+      it "assigns the requested user to @user" do
+        user = create(:user)
+        get :show, params: { id: user }
+        expect(assigns(:user)).to eq(user)
       end
     end
   end

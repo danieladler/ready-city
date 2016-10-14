edc391b90f33f8230d72e2de49c100cf9f6d82e8
danieladler <weston.adler@gmail.com>
2016-10-14 14:05:55 -0700

    refactor user controller specs to utilize factories & kwargs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,32 +9,34 @@ describe UsersController, type: :controller do
   end
 
   describe "POST #create" do
-  #   let(:make_request!) {post :create, params: {user: {username: "user_0", email:"user_0@user.com", password: "pw", password_confirmation: "pw"}}}
-  #
-  #     context "new User is valid" do
-  #       it "redirects to root" do # TODO: redirect to new user's account show page instead
-  #         make_request!
-  #         expect(response).to redirect_to root_path
-  #       end
-  #
-  #       it "returns a descriptive welcome message" do
-  #         make_request!
-  #         expect(flash[:success]).to eq "Account created. Welcome to Ready City!"
-  #       end
-  #     end
-  #
-  #     context "new User is invalid" do
-  #       let(:make_request!) {post :create, params: { user: {username: "user_2", email:"user_2@user.com"} } }
-  #
-  #       it "renders /sign-up template" do
-  #         make_request!
-  #         expect(response).to render_template :sign_up
-  #       end
-  #
-  #       it "returns a descriptive error message" do
-  #         make_request!
-  #         expect(flash[:error]).to include "username: has already been taken"
-  #       end
-  #     end
+    context "with valid attributes" do
+      it "creates a new user" do
+        expect {
+          post :create, params: {user: attributes_for(:user)}
+        }.to change(User, :count).by(1)
+      end
+
+      it "redirects to the new user" do
+        post :create, params: {user: attributes_for(:user)}
+        expect(response).to redirect_to User.last
+      end
+
+      it "returns a descriptive success message" do
+        post :create, params: {user: attributes_for(:user)}
+        expect(flash[:success]).to include "Account created. Welcome to Ready City!"
+      end
+    end
+
+    context "with invalid attributes" do
+      it "renders /sign-up template" do
+        post :create, params: {user: attributes_for(:user, username: nil)}
+        expect(response).to render_template :sign_up
+      end
+
+      it "returns a descriptive error message" do
+        post :create, params: {user: attributes_for(:user, username: nil)}
+        expect(flash[:error]).to include "username: can't be blank"
+      end
+    end
   end
 end

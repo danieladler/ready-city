249faf99ffbff93176584987e5a04a3de459a6f5
danieladler <weston.adler@gmail.com>
2016-10-14 12:39:40 -0700

    change user specs to match validation error messages

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,32 +4,37 @@ describe User, type: :model do
   subject(:user) {create(:user)}
 
   context "is invalid" do
+    # TODO: figure out if there's a way to test for error messages in a one-liner
+    #       spec that is roughly: expect(new_user.valid?).to include([error message])
+
     it "when email is empty" do
       user.email = nil
-      expect(user.valid?).to eq false
+      user.valid?
+      expect(user.errors.messages[:email]).to include("can't be blank")
     end
 
     it "when email is not unique" do
       new_user = build(:user, email: user.email)
-      expect(new_user.valid?).to eq false
+      new_user.valid?
+      expect(new_user.errors.messages[:email]).to include("has already been taken")
     end
 
     it "when email is missing '@'" do
       user.email = "user at google dot com"
-      expect(user.valid?).to eq false
+      user.valid?
+      expect(user.errors.messages[:email]).to include("is invalid")
     end
 
     it "when username is empty" do
       user.username = nil
-      expect(user.valid?).to eq false
+      user.valid?
+      expect(user.errors.messages[:username]).to include("can't be blank")
     end
 
     it "when username not unique" do
       new_user = build(:user, username: user.username)
       new_user.valid?
       expect(new_user.errors.messages[:username]).to include("has already been taken")
-      # TODO: figure out if there's a way to test for above error message in a
-      #       one-liner spec that is roughly: expect(new_user.valid?).to include([error message])
     end
   end
 end

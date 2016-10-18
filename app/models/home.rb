76f1036ee72f6232b38ea25a22de96b63ea38247
danieladler <weston.adler@gmail.com>
2016-10-18 10:32:36 -0700

    TDD Home model validations

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,4 +1,9 @@
 class Home < ApplicationRecord
   belongs_to :user, optional: true
-  # validates_presence_of addresses, etc.
+  validates_presence_of :zip, :user
+      # NB: chose to validate zip but not other address fields as a user may
+      # not want to share their full address, but they should be required to
+      # share zip code so that app can provide information relevant to their
+      # general vicinity.
+
 end
--- spec/models/home_spec.rb
+++ spec/models/home_spec.rb
@@ -4,15 +4,22 @@ describe Home, type: :model do
   subject(:home) {create(:home)}
 
   describe ".validates" do
-    context "is valid" do
-      it "responds" do
-        puts home.inspect
-        puts home.user.inspect
+    context "is invalid" do
+      it "when is not associated with a user" do
+        home.user = nil
+        expect(home.valid?).to eq false
       end
+
+      it "when zip code is missing" do
+        home.zip = nil
+        home.valid?
+        expect(home.errors.messages[:zip]).to include("can't be blank")
+      end
+
+      # TODO: add specs in this context for existince of has_many preps
     end
 
-    # replace the above with context "is invalid" and TDD-out Home model validations
-    # add context: is house
-    # add context: is apartment
+    # TODO: add context: is house
+    # TODO: add context: is apartment
   end
 end

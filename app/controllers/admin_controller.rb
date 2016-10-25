8a871d7067fe79770bce05a1cea4861917ab232b
danieladler <weston.adler@gmail.com>
2016-10-24 20:45:36 -0700

    add Admin controller and admin column to Users

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,8 @@
+class AdminController < ApplicationController
+  before_action :require_admin
+
+  def require_admin
+    redirect_to root_path if !current_user || !current_user.admin?
+  end
+  
+end

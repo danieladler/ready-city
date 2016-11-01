84c7976d8c01f8f2a9e5c72269eaa2020b6f96ac
danieladler <weston.adler@gmail.com>
2016-11-01 13:32:48 -0700

    update factories and keep writing specs for dependent_assessment_controller

--- spec/controllers/dependent_assessment_controller_spec.rb
+++ spec/controllers/dependent_assessment_controller_spec.rb
@@ -5,12 +5,20 @@ describe DependentAssessmentController, type: :controller do
 
   describe "POST #create" do
     let(:user) { create(:user) }
+
     before(:each) do
       stub_current_user(user)
     end
 
     context "with invalid attributes" do
+      let(:make_request) {
+        post :create, params: {dependent: attributes_for(:dependent, :human, name: nil)}
+      }
 
+      it "renders a descriptive error message" do
+        make_request
+        expect(flash[:error]).to include "name: can't be blank"
+      end
     end
 
     context "with valid attributes; Dependent is a HUMAN" do
@@ -23,29 +31,45 @@ describe DependentAssessmentController, type: :controller do
       end
 
       it "returns a descriptive success message" do
-
+        make_request
+        expect(flash[:success]).to include "Dependent Added"
       end
 
       it "redirects to current_user's profile" do
-
+        make_request
+        expect(response).to redirect_to user_path(user.id)
       end
 
       it "generates gear_human gear preps" do
-
+        preparation_gear_human = create(:gear_human)
+        make_request
+        expect(user.user_preps.where(prep_subtype: 'gear_human').count).to eq 1
       end
 
       it "does not generate any gear_pet gear preps" do
-
+        make_request
+        expect(user.user_preps.where(prep_subtype: 'gear_pet').count).to eq 0
       end
     end
 
     context "with valid attributes; Dependent is a PET" do
-      it "generates gear_pet gear preps" do
+      let(:make_request) {
+        post :create, params: {dependent: attributes_for(:dependent, :pet)}
+      }
 
+      it "generates gear_pet gear preps" do
+        preparation_gear_pet =  create(:gear_pet)
+        make_request
+        expect(user.user_preps.where(prep_subtype: 'gear_pet').count).to eq 1
+      end
+
+      it "does not generate any gear_human gear preps" do
+        make_request
+        expect(user.user_preps.where(prep_subtype: 'gear_human').count).to eq 0
       end
     end
 
-    context "with valid attributes; two PET Dependents" do
+    context "with valid attributes; two PET Dependents" do # This context is to test that the total_cost_in_cents of a given user_prep is updated based on qty of dependents
       it "passes in correct # of Pets for consumer_multiplier as
       reflected in double quantities of gear_pet gear preps" do
 
--- spec/factories/preparations.rb
+++ spec/factories/preparations.rb
@@ -6,10 +6,27 @@ FactoryGirl.define do
     base_cost_in_cents 100
 
     factory :home_prep do
-      prep_subtype "home"
+      prep_maintype "home"
+      prep_subtype "home_structure"
       keyword "foundation"
       instructions "anchor home frame to foundation"
       base_cost_in_cents 400000
+    end
+
+    factory :gear_human do
+      prep_maintype "gear"
+      prep_subtype "gear_human"
+      keyword "map"
+      instructions "local map"
+      base_cost_in_cents 500
+    end
+
+    factory :gear_pet do
+      prep_maintype "gear"
+      prep_subtype "gear_pet"
+      keyword "pet_food"
+      instructions "nonperishable pet food"
+      base_cost_in_cents 200
     end
   end
 end

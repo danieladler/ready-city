360e8b5a3da8f1e68742818b5cb013fcc450b7e5
danieladler <weston.adler@gmail.com>
2016-11-03 14:40:34 -0700

    add Preparation.import functionality to update Preps from external db via CSV

--- app/models/preparation.rb
+++ app/models/preparation.rb
@@ -1,4 +1,16 @@
 class Preparation < ApplicationRecord
   validates_presence_of :prep_maintype, :keyword, :instructions
   validates_uniqueness_of :keyword, :instructions
+  require 'csv'
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      if Preparation.where(keyword: row["keyword"]).exists? == false
+        Preparation.create row.to_hash
+      else
+        @preparation = Preparation.find_by(keyword: row["keyword"])
+        @preparation.update(row.to_hash)
+      end
+    end
+  end
 end

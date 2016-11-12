c70e2dee90e794abca7cd3d36000d3300d0d70b6
danieladler <weston.adler@gmail.com>
2016-11-11 16:27:47 -0800

    abstract reconcile user_prep code into own method; remove commented notes & pseudocode

--- app/controllers/admin/prep_record_manager_controller.rb
+++ app/controllers/admin/prep_record_manager_controller.rb
@@ -1,16 +1,11 @@
 class Admin::PrepRecordManagerController < ApplicationController
   def import_and_reconcile
     import_preps_from_CSV
-    # reconcile_user_preps
     flash[:success] = "Preparations Updated"
     redirect_to admin_preparations_path
   end
 
   def import_preps_from_CSV
     PrepRecordManager.import(params[:file])
-  end
-
-  def reconcile_user_preps
-    PrepRecordManager.reconcile
   end
 end
--- app/models/prep_record_manager.rb
+++ app/models/prep_record_manager.rb
@@ -44,9 +44,7 @@ class PrepRecordManager
       if @preparation = Preparation.find_by(tracker_id: row["tracker_id"]) # if this Preparation already exists in db...
         @preparation.attributes = row.to_hash # ...resassign its attributes from the data in the CSV row...
         if @preparation.changed? # ...and if its attrs were changed in this process...
-          UserPrep.where(prep_id: @preparation.id).each do |up| # ...find UserPreps matching this Prep...
-            up.update_attrs_from_preparation(@preparation.attributes) # ...and update their attrs with all the new attrs of the updated Prep.
-          end
+          reconcile_user_preps(@preparation)
         end
         @preparation.save # then go ahead and save the Preparation with its new attrs.
       else
@@ -55,55 +53,9 @@ class PrepRecordManager
     end
   end
 
-
-
-  # def self.reconcile
-  #
-  # end
-
-  # def self.reconcile
-  # # GOAL: fix UserPreps where keywords are outdated.
-  #   # These will be created whenever Preparations' keywords are updated
-  #   # Any NEW UserPreps after Prep import will be fine because they'll be generated from the latest Preparation keywords
-  #   # But existing UserPreps, which were created based on shared keyword w/ Preps, will be 'orphaned'
-  #   # These UserPreps need to be kept because they'll still contain valuable information & user-specific notes
-  #   # So how to find & correct them?
-  #
-  #   load_preparation_keywords(@preparation_keywords = []) # create array of Preparation keywords from DB
-  #   load_userprep_keywords(@user_prep_keywords = [])      # create array of UserPrep keywords for all UserPreps in DB
-  #   compare_prep_to_user_prep_keywords(@preparation_keywords, @user_prep_keywords, @user_prep_keywords_to_fix = nil) # compare the two arrays from CSV & DB, returning modified user_prep_keywords_to_fix array
-  #   if @user_prep_keywords_to_fix != nil                  # if above method does NOT return nil, meaning there are actually UserPreps with outdated keywords...
-  #     update_old_user_prep_keywords(@user_prep_keywords_to_fix) # ...update them to match keywords (& content) from the latest Preparations in DB
-  #   end
-  # end
-
-  # def self.load_preparation_keywords(preparation_keywords)
-  #   Preparation.all.each { |p| @preparation_keywords.push(p.keyword) }
-  # end
-  #
-  # def self.load_userprep_keywords(user_prep_keywords)
-  #   UserPrep.all.each {|up| @user_prep_keywords.push(up.keyword)}
-  # end
-  #
-  # def self.compare_prep_to_user_prep_keywords(preparation_keywords, user_prep_keywords, user_prep_keywords_to_fix)
-  #   if (@user_prep_keywords - @preparation_keywords).empty? == false # if there are UserPreps w/ keywords that don't match what's in db...
-  #     @user_prep_keywords_to_fix = @user_prep_keywords - @preparation_keywords # ...then they're put in a new array which is returned to the parent
-  #   end
-  # end
-  #
-  # def self.update_old_user_prep_keywords(user_prep_keywords_to_fix)
-  #   # raise
-  #   @user_prep_keywords_to_fix.each do |k|      # for each outdated keyword in the array...
-  #     UserPrep.where(keyword: k).each do |up|   # ...find all UserPrep records with that outdated keyword; for each of these...
-  #       p = Preparation.find_by(id: up.prep_id) # ...identify the Preparation record with the matching prep_id (since keyword will not be matching); this Preparation should contain the updated keyword & content.
-  #       raise if Preparation.find_by(id: up.prep_id) == nil
-  #         # If this raise happens, it means a UserPrep's parent Preparation was destroyed.
-  #         # In this case, just manually figure out if the Preparation has been recreated.
-  #           # IF YES, then update the UserPrep's prep_id to match the Preparation
-  #           # IF NO, then the UserPrep may not be needed since its parent Preparation was destroyed.
-  #         # NB: for now, just handle this all manually since these cases should be rare.
-  #       up.update(keyword: p.keyword) # However, if a match IS found, then update the UserPrep's keyword to match the Preparation's keyword.
-  #     end
-  #   end
-  # end
+  def self.reconcile_user_preps(preparation)
+    UserPrep.where(prep_id: @preparation.id).each do |up| # ...find UserPreps matching this Prep...
+      up.update_attrs_from_preparation(@preparation.attributes) # ...and update their attrs with all the new attrs of the updated Prep.
+    end
+  end
 end

ad3dba9116ce8de31b8ec1b4101ff53fd279b344
danieladler <weston.adler@gmail.com>
2016-11-15 09:13:47 -0800

    add creation of UserPreps with prep_maintype: plan_home

--- app/controllers/home_assessment_controller.rb
+++ app/controllers/home_assessment_controller.rb
@@ -25,5 +25,6 @@ class HomeAssessmentController < ApplicationController
     @pb.generate_preps("home_interior") # generic stage:before home preps regardless of house/apt/etc.
     @pb.generate_preps("home_structure") if @home.is_house # home preps that a homeowner can work on but renter likely cannot.
     @pb.generate_preps("home_check")    # generic stage:after preps for all home types
+    @pb.generate_preps("plan_home")
   end
 end

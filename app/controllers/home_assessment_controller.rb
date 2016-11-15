dbbc5805b15c623c4778c29e7a4c6f2dafc77f9b
danieladler <weston.adler@gmail.com>
2016-11-15 14:12:56 -0800

    destroy user_preps focusing on @home.is_house if user's home is switched from house to other

--- app/controllers/home_assessment_controller.rb
+++ app/controllers/home_assessment_controller.rb
@@ -2,8 +2,8 @@ class HomeAssessmentController < ApplicationController
   def update_home
     @home ||= Home.load_home(current_user)
     @home.update_db_values(params)
+    destroy_house_preps if !@home.is_house # have to call this before @save in order to return a value from .changed method below
     if @home.save
-      generate_home_preps(current_user, @home)
       flash[:success] = "Home Updated"
       redirect_to user_path(current_user.id) # TODO: replace redirect w/ AJAX
     elsif @home.errors
@@ -26,5 +26,13 @@ class HomeAssessmentController < ApplicationController
     @pb.generate_preps("home_structure") if @home.is_house # home preps that a homeowner can work on but renter likely cannot.
     @pb.generate_preps("home_check")    # generic stage:after preps for all home types
     @pb.generate_preps("plan_home")
+    destroy_house_preps if !@home.is_house?
+  end
+
+  def destroy_house_preps
+    if @home.changed == ["is_house"]
+      up = UserPrep.where(user_id: current_user.id, prep_subtype: 'home_structure')
+      up.destroy_all
+    end
   end
 end

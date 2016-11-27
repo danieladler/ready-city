d400314aa665011ca8efc10f83e0e4bf2fae4ace
danieladler <weston.adler@gmail.com>
2016-11-27 09:16:11 -0800

    confirm dynamic data rendered via React component

---
+++ app/assets/javascripts/components/users/home_assessment.js.jsx
@@ -0,0 +1,14 @@
+var HomeAssessment = React.createClass({
+  // getInitialState: function() {
+  //   return {}
+  // },
+  render: function() {
+    return (
+      <div>
+        <h1> JSX! </h1>
+        <h2> Data: {this.props.home.city} </h2>
+      </div>
+    )
+    ;
+  }
+});

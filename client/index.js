bfefc88fc4a5aed52fee8e451e2806f48e9f87ac
danieladler <weston.adler@gmail.com>
2016-12-23 17:40:19 -0800

    add a test React component to confirm it's visible; move webpack-react script into only the page where it's needed in a hacky attempt to render React without DOM errors

--- client/index.js
+++ client/index.js
@@ -1,5 +1,12 @@
-import test from './components/test';
+// dependencies
+import React from 'react';
+import ReactDOM from "react-dom";
+// components
+import App from './startup/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root-assessment')
+);
 
 console.log("Hello world!");
-
-test();

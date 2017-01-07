e5df1f91d5d208e29eb8ccea9be691f188a949f4
danieladler <weston.adler@gmail.com>
2017-01-07 10:42:26 -0800

    Start creating AddZoneForm, log its values onSubmit

---
+++ client/components/zones/CreateZoneForm.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Field, reduxForm } from 'redux-form'
+
+class CreateZoneForm extends React.Component {
+  render() {
+    const renderErrors = (errors) => {
+      const mapped = errors.map(function(error, index) {
+        return(<p key={index}><strong>{error}</strong></p>);
+      });
+      return(mapped);
+    }
+
+    const { handleSubmit, zones } = this.props;
+    const token = $('meta[name="csrf-token"]').attr('content');
+
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input type="hidden" name="authenticity_token" value={token} readOnly={true} />
+          <div>
+            <label>Add a new zone</label>
+            <div>
+              <label>Name</label>
+              <Field name="name" component="input" type="text" placeholder="name"/>
+            </div>
+            <div>
+              <label>Address</label>
+              <Field name="address" component="input" type="text" placeholder="address"/>
+            </div>
+            <div>
+              <label>City</label>
+              <Field name="city" component="input" type="text" placeholder="city"/>
+            </div>
+            <div>
+              <label>State</label>
+              <Field name="state" component="input" type="text" placeholder="state"/>
+            </div>
+            <div>
+              <label>Zip</label>
+              <Field name="zip" component="input" type="text" placeholder="zip"/>
+            </div>
+          </div>
+          <div>
+            <label>What type of zone is this?</label>
+            <Field ref="ZoneType" name="zone_type" component="select">
+              <option value="">[select]</option>
+              <option value="zone_home">Home</option>
+              <option value="zone_work">Work</option>
+            </Field>
+          </div>
+          <div className='message-container' data-zone-id='create-zone-error'>
+            { zones.errors ? renderErrors(zones.errors) : null }
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default reduxForm({
+  form: 'CreateZoneForm'
+})(CreateZoneForm)
--- client/components/zones/ZoneAssessment.jsx
+++ client/components/zones/ZoneAssessment.jsx
@@ -1,7 +1,19 @@
 import React, { PropTypes } from 'react';
 import ZoneInstance from './ZoneInstance.jsx';
+import CreateZoneForm from './CreateZoneForm.jsx';
 
 class ZoneAssessment extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(params) {
+    console.log(params);
+    debugger
+    // this.props.createContact(params);
+  }
+
   render() {
     var zones = this.props.zones.all.map(function(zone, index) {
       return (
@@ -17,6 +29,7 @@ class ZoneAssessment extends React.Component {
     <div id="zone-assessment-wrapper">
       <h2> Zones: </h2>
       { zones }
+      <CreateZoneForm {...this.props} onSubmit={this.handleSubmit} />
     </div>
   )}
 }

e64b5895e54b8f6a96bafdb389ceecaac9952167
danieladler <weston.adler@gmail.com>
2017-01-20 13:13:09 -0800

    add background filter & fix resize display behavior for mobile nav menu

--- app/assets/javascripts/shame.js
+++ app/assets/javascripts/shame.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
   $('.toggle-mobile').click(function(){
     $('.menu-nav-mobile').toggleClass('inactive');
+    $('.dark-background-filter').toggleClass('inactive');
   })
+});
+
+$(window).resize(function() {
+  var windowWidth = $(window).width();
+  if (windowWidth > 768) {
+    $('.menu-nav-mobile').addClass('inactive');
+    $('.dark-background-filter').addClass('inactive');
+  }
 });

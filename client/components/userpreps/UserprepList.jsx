d5d55960233a01e1f2d59ceafc07f3b555a1e02b
danieladler <weston.adler@gmail.com>
2017-01-27 16:50:45 -0800

    add notes to userpreps view and /about

--- client/components/userpreps/UserprepList.jsx
+++ client/components/userpreps/UserprepList.jsx
@@ -15,6 +15,15 @@ class UserprepList extends React.Component {
 
     return (
       <div>
+        <h1 className="alert-temp alert-error"> Hello!! </h1>
+        <h2 className="alert-temp">
+          This page is a work in progress. It's not very pretty right now but it
+          will soon become the centerpiece of the app.
+        </h2>
+        <h2 className="alert-temp">
+          Check the latest commits in the <a href="https://www.github.com/danieladler/ready-city">
+          Ready City repo </a> for an idea of upcoming updates.
+        </h2>
         {userpreps}
       </div>
     )

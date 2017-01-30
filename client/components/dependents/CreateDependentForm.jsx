7c57c00c2dbf84bc20b1d7acc2ebcf54d0e774db
danieladler <weston.adler@gmail.com>
2017-01-30 11:30:47 -0800

    add expander for CreateDependentForm

--- client/components/dependents/CreateDependentForm.jsx
+++ client/components/dependents/CreateDependentForm.jsx
@@ -5,6 +5,8 @@ class CreateDependentForm extends React.Component {
   constructor(props, context) {
     super(props, context)
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.toggleExpander = this.toggleExpander.bind(this)
+    this.state = {expanderHidden: "expander-hidden"};
   }
 
   handleFormSubmit(event) {
@@ -21,11 +23,19 @@ class CreateDependentForm extends React.Component {
     this.props.createDependent(params, userId);
   }
 
+  toggleExpander(event) {
+    event.preventDefault();
+    if (this.state.expanderHidden == "expander-hidden") {
+      this.setState({expanderHidden: "expander-showing"})
+    } else {
+      this.setState({expanderHidden: "expander-hidden"})
+    }
+  }
+
   render() {
     const renderErrors = (errors) => {
       const mapped = errors.map((error, index) => {
         return(<p key={index} className="alert alert-error">{error}</p>);
-;
       });
       return(mapped);
     }
@@ -35,28 +45,32 @@ class CreateDependentForm extends React.Component {
 
     return (
       <div className="wrapper-create-form">
-        <form onSubmit={this.handleFormSubmit} className="form form-assessment form-create-instance">
+        <a href="#" className={"expander-trigger " + this.state.expanderHidden} onClick={this.toggleExpander}>
           <h3 className="title title-form title-form-section">Add a new dependent</h3>
-          <input type="hidden" ref="Token" name="authenticity_token" value={token} readOnly={true} />
-          <div className="form-group">
-            <label className="label-assessment-form"> Name </label>
-            <Field ref="Name" name="name" component="input" type="text" className="input-assessment-form"/>
-          </div>
-          <div className="form-group">
-            <label className="label-assessment-form">Human or Pet?</label>
-            <Field ref="Human" name="human" component="select">
-              <option value="">[Select]</option>
-              <option value="true">Human</option>
-              <option value="false">Pet</option>
-            </Field>
-          </div>
-          <div className='alert-container' data-dependent-id='create-dependent-error'>
-            { dependents.errors ? renderErrors(dependents.errors) : null }
-          </div>
-          <div>
-            <button type="submit" className="button button-form button-submit">Submit</button>
-          </div>
-        </form>
+        </a>
+        <div className="expander-content">
+          <form onSubmit={this.handleFormSubmit} className="form form-assessment form-create-instance">
+            <input type="hidden" ref="Token" name="authenticity_token" value={token} readOnly={true} />
+            <div className="form-group">
+              <label className="label-assessment-form"> Name </label>
+              <Field ref="Name" name="name" component="input" type="text" className="input-assessment-form"/>
+            </div>
+            <div className="form-group">
+              <label className="label-assessment-form">Human or Pet?</label>
+              <Field ref="Human" name="human" component="select">
+                <option value="">[Select]</option>
+                <option value="true">Human</option>
+                <option value="false">Pet</option>
+              </Field>
+            </div>
+            <div className='alert-container' data-dependent-id='create-dependent-error'>
+              { dependents.errors ? renderErrors(dependents.errors) : null }
+            </div>
+            <div>
+              <button type="submit" className="button button-form button-submit">Submit</button>
+            </div>
+          </form>
+        </div>
       </div>
     )
   }

c84d46fb6e3e716174ae00fa32b73f6951e1d9cf
danieladler <weston.adler@gmail.com>
2017-01-31 15:29:41 -0800

    drop in bourbon-refills styling for breadcrumbs at top of userprep left-nav for priority sections

--- client/components/userpreps/UserprepLeftNav.jsx
+++ client/components/userpreps/UserprepLeftNav.jsx
@@ -4,6 +4,12 @@ class UserprepLeftNav extends React.Component {
   render() {
     return (
       <div className="userprep-left-nav">
+        <h2 className="title-userprep-breadcrumbs"> Priority: </h2>
+        <div className="breadcrumbs">
+          <a href="javascript:void(0)">1</a>
+          <a href="javascript:void(0)">2</a>
+          <a href="javascript:void(0)">3</a>
+        </div>
         <h1> Left Nav! </h1>
       </div>
     )

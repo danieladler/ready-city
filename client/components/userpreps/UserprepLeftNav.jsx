c0b17fdd3d32a356cf3c46777cf62cdc0d7de1c0
danieladler <weston.adler@gmail.com>
2017-01-31 17:16:32 -0800

    line up prep view simple styling before adding Redux filtering logic

--- client/components/userpreps/UserprepLeftNav.jsx
+++ client/components/userpreps/UserprepLeftNav.jsx
@@ -1,17 +1,45 @@
 import React, { PropTypes } from 'react';
 
 class UserprepLeftNav extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.filterUserpreps = this.filterUserpreps.bind(this);
+  }
+
+  filterUserpreps(event) {
+    event.preventDefault();
+    console.log('yep');
+  }
+
   render() {
     return (
-      <div className="userprep-left-nav">
+      <nav className="nav-left-userpreps">
         <h2 className="title-userprep-breadcrumbs"> Priority: </h2>
         <div className="breadcrumbs">
           <a href="javascript:void(0)">1</a>
           <a href="javascript:void(0)">2</a>
           <a href="javascript:void(0)">3</a>
         </div>
-        <h1> Left Nav! </h1>
-      </div>
+        <ul className="list-userprep-maintypes">
+          <li className="item-userprep item-userprep-maintype">
+            <a href="javascript:void(0)" onClick={this.filterUserpreps}>Home</a>
+          </li>
+            <ul className="list-userprep-subtypes">
+              <li className="item-userprep item-userprep-subtype">Structure</li>
+              <li className="item-userprep item-userprep-subtype">Interior</li>
+            </ul>
+          <li className="item-userprep item-userprep-maintype">Gear</li>
+            <ul className="list-userprep-subtypes">
+              <li className="item-userprep item-userprep-subtype">Human</li>
+              <li className="item-userprep item-userprep-subtype">Pet</li>
+            </ul>
+          <li className="item-userprep item-userprep-maintype">Plan</li>
+            <ul className="list-userprep-subtypes">
+              <li className="item-userprep item-userprep-subtype">TBD</li>
+              <li className="item-userprep item-userprep-subtype">TBD</li>
+            </ul>
+        </ul>
+      </nav>
     )
   }
 }
